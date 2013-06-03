51bf185ce8d068566422b8722c86195e146cdfa6
cardoppler <cardoppler@gmail.com>
2013-06-03 20:49:28 +0000

    modified:   ../logger/logger.py

--- sensible_data_service/logger/logger.py
+++ sensible_data_service/logger/logger.py
@@ -1,1 +1,11 @@
-# test
+from pymongo import MongoClient
+import service_config
+import SECURE_service_config
+from utils.log_database import LogDatabase
+
+class Logger(object):
+
+	logDatabase = None
+
+	def __init__(self):
+			self.logDatabase = LogDatabase()
