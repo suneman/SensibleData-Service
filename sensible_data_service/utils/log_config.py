2881deb7b4f2d2926a88eaf63e219f47e005739c
cardoppler <cardoppler@gmail.com>
2013-06-05 16:08:05 +0000

    new file:   logger_manager/Helper.py 	new file:   utils/log_config.py

---
+++ sensible_data_service/utils/log_config.py
@@ -0,0 +1,33 @@
+
+LOGGER_DATABASE = {
+	"backend":"mongodb",
+	"params": {
+		"url":"mongodb://%s:%s@ds029338.mongolab.com:29338/sensible-logger",
+		"database":"sensible-logger",
+		"collection":"logger-collection",
+		"username":"logger",
+		"password":"logger"
+	}
+}
+
+
+
+
+
+
+
+PATH_TO_SECRET = "secret.txt"
+
+D0 = {
+	"appID": " ",
+	"payload": " ",
+	"userID": " ",
+	"userAppFlow" : " "
+}
+
+SEED = "THISISTHEPUBLICSEED"
+A0 = "ABCDEFGHABCDEFGHABCDEFGHABCDEFGH"
+
+DIGEST_MODE = "SHA512Hash"
+DIGEST_SIZE = "64"
+
